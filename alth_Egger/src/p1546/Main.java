7864b36c092fa7fd9be46b0b5d558c02c9f1bdce
yoon <cbj0010@naver.com>
2024-08-07 23:17:35 +0900

    Num : 1546
    평균

---
+++ alth_Egger/src/p1546/Main.java
@@ -0,0 +1,32 @@
+package p1546;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int inputLength = Integer.parseInt(bf.readLine());
+
+        float score[] = new float[inputLength];
+        st = new StringTokenizer(bf.readLine());
+        float resultScore = 0;
+
+        for (int i = 0; i < score.length; i++) {
+            score[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(score);
+
+        float maxScore = score[score.length-1];
+
+        for(Float i:score) {
+            resultScore +=i/maxScore*100;
+        }
+        System.out.println(resultScore/score.length);
+    }
+}
