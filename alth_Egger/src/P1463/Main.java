fb14bea065fc7fe8b459e54540b839e63c02e1ab
yoon <cbj0010@naver.com>
2024-08-09 17:51:37 +0900

    Num : 1463
    dp

---
+++ alth_Egger/src/P1463/Main.java
@@ -0,0 +1,22 @@
+package P1463;
+
+import java.util.Scanner;
+
+public class Main {
+    static Integer[] dp;
+
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        dp = new Integer[N + 1];
+        dp[0] = dp[1] = 0;
+
+        for (int i = 2; i < N; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        System.out.println(dp[N]);
+    }
+}
