2a556162f30c3753ce3f352b748738ffd44783f2
yoon <cbj0010@naver.com>
2024-08-12 01:59:00 +0900

    2839
    좀 더 쉬운(?)방법으로 풀어보기

--- alth_Egger/src/p2839/Main.java
+++ alth_Egger/src/p2839/Main.java
@@ -1,2 +1,25 @@
-package p2839;public class Main {
+package p2839;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int sugar = scn.nextInt();
+        int result = 0;
+
+        while (sugar > 0) {
+            if (sugar % 5 == 0) {
+                result += sugar / 5;
+                break;
+            } else {
+                sugar -= 3;
+                result++;
+            }
+            if (sugar < 0) {
+                result = -1;
+            }
+        }
+        System.out.println(result);
+    }
 }
