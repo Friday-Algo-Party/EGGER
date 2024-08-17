6b844e697d73b20da7a718b4db81cbacd5d756be
yoon <cbj0010@naver.com>
2024-08-17 22:12:46 +0900

    3052_나머지
    Hash set
    Hash Set은 중복을 제거해주는 착한 친구네

--- alth_Egger/src/class1/p3052_나머지/main.java
+++ alth_Egger/src/class1/p3052_나머지/main.java
@@ -3,17 +3,16 @@ package class1.p3052_나머지;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+import java.util.HashSet;
 
 public class main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> minList = new ArrayList<>();
+        HashSet<Integer> minList = new HashSet<>();
         for (int i = 0; i < 10; i++) {
             int inputNum = Integer.parseInt(br.readLine());
             minList.add(inputNum % 42);
         }
-        System.out.println(minList.stream().distinct().collect(Collectors.toList()).size());
+        System.out.println(minList.size());
     }
 }
