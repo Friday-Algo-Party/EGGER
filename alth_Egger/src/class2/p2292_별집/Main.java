ca391c5198ba77d0d569cffcfe968207ab49e08e
3-34\3-34 <cbj0010@naver.com>
2024-08-22 20:21:53 +0900

    while로 고치기

--- alth_Egger/src/class2/p2292_별집/Main.java
+++ alth_Egger/src/class2/p2292_별집/Main.java
@@ -1,25 +1,19 @@
 package class2.p2292_별집;
 
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int N = scn.nextInt();
-        int result =1;
-        for (int i = 1; i <N ; i++) {
+        int count = 1; // 층 수 카운트
+        int result = 1; // 방의 최대 개수
 
-            if(N/(6*i)<2) {
-                System.out.println(i);
-                if (i<=2) {
-                    result = i+1;
-                    break;
-                }
-                result = i;
-                break;
-            }
+        while (result < N) {
+            count++;
+            result += 6 * (count - 1);
         }
-        System.out.println(result);
+
+        System.out.println(count);
     }
 }
