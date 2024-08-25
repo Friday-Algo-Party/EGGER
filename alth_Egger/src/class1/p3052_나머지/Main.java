275191f0c2e28f4d365fb54c710ffdeb1c45ccce
yoon <cbj0010@naver.com>
2024-08-25 19:47:16 +0900

    class1
    rollback

--- alth_Egger/src/class1/p3052_나머지/Main.java
+++ alth_Egger/src/class1/p3052_나머지/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 
-public class main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Integer> minList = new HashSet<>();
