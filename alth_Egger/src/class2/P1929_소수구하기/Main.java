63936d383a90cf964be2c224f09ab87be4b9d2cf
yoon <cbj0010@naver.com>
2024-09-21 16:33:33 +0900

    1929 소수구하기

---
+++ alth_Egger/src/class2/P1929_소수구하기/Main.java
@@ -0,0 +1,33 @@
+package class2.P1929_소수구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        for (int i = M; i < N + 1; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;   //1보다 작거나 같은건 소수가 아님
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// 감자넘 지피티 그만 사용해...!
