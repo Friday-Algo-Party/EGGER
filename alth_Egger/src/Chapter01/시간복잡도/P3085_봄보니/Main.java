e7728c3c88fbb3a85d934b189c8f15e6a8dc6b22
yoon <cbj0010@naver.com>
2025-02-02 03:09:42 +0900

    3085 문제의 봄보닝
    이게 맞나.? 찐 쌩 구현
    브루드포스트의 정석이다 진짜

--- alth_Egger/src/Chapter01/시간복잡도/P3085_봄보니/Main.java
+++ alth_Egger/src/Chapter01/시간복잡도/P3085_봄보니/Main.java
@@ -18,60 +18,66 @@ public class Main {
         //여까지가 입력
         int checkNum = 0;
 
-        //가로로 바꾸기
+        //가로와 세로 모두 확인하는 방법
         for (int i = 0; i < length; i++) {
             for (int j = 1; j < length; j++) {
-                if (!candy[i][j].equals(candy[j][j - 1])) {
+                // 가로로 교환
+                if (!candy[i][j].equals(candy[i][j - 1])) {
                     Character[][] copyCandy = deepCopy(candy);
-                    char temp = candy[i][j];
-                    copyCandy[i][j] = candy[i][j - 1];
-                    candy[i][j - 1] = temp;
+                    char temp = copyCandy[i][j];
+                    copyCandy[i][j] = copyCandy[i][j - 1];
+                    copyCandy[i][j - 1] = temp;
+                    int checkNextNum = checkContinue(copyCandy);
+                    if (checkNum < checkNextNum) checkNum = checkNextNum;
+                }
+
+                // 세로로 교환 (위아래로도 비교)
+                if (i > 0 && !candy[i][j].equals(candy[i - 1][j])) {  // 세로로 교환 시
+                    Character[][] copyCandy = deepCopy(candy);
+                    char temp = copyCandy[i][j];
+                    copyCandy[i][j] = copyCandy[i - 1][j];
+                    copyCandy[i - 1][j] = temp;
                     int checkNextNum = checkContinue(copyCandy);
                     if (checkNum < checkNextNum) checkNum = checkNextNum;
                 }
             }
             if (checkNum == candy.length) break;
         }
+
         System.out.println(checkNum);
     }
 
     public static int checkContinue(Character candy[][]) { //몇개가 연속인지 확인하는 메서드
+        int maxCount = 1;
+        int length = candy.length;
 
-        int xResult = 1;
-        int yResult = 1;
         //가로 적으로 연속적인거
-        for (int i = 0; i < candy.length; i++) {
-            int pCount = 1;
-            int cCount = 1;
+        for (int i = 0; i < length; i++) {
+            int count = 1;
 
-            for (int j = 0; j < candy[i].length; j++) {
-                if (candy[i][0] == 'C' && candy[i][cCount - 1] == 'C') {
-                    cCount++;
-                } else if (candy[i][0] == 'P' && candy[i][cCount - 1] == 'P') {
-                    pCount++;
-                }
-            }
-            if (Math.max(pCount, cCount) > xResult) xResult = Math.max(pCount, cCount);
-        }
-
-        //세로적으로 연속적인거
-        for (int i = 0; i < candy.length; i++) {
-            int pCount = 1;
-            int cCount = 1;
             for (int j = 1; j < candy[i].length; j++) {
-                if (candy[j][i] == 'C' && candy[j][j - 1] == 'C') {
-                    cCount++;
-                } else if (candy[j][i] == 'P' && candy[j][j - 1] == 'P') {
-                    pCount++;
-                }
+                if (candy[i][j].equals(candy[i][j - 1])) {
+                    count++;
+                    maxCount = Math.max(maxCount, count);
+                } else count = 1;
             }
-            if (Math.max(pCount, cCount) > yResult) yResult = Math.max(pCount, cCount);
         }
 
-
+        // 세로 연속 체크
+        for (int j = 0; j < length; j++) {
+            int count = 1;
+            for (int i = 1; i < length; i++) {
+                if (candy[i][j] == candy[i - 1][j]) {
+                    count++;
+                    maxCount = Math.max(maxCount, count);
+                } else {
+                    count = 1;
+                }
+            }
+        }
         //p가 연속적으로 있는 것의 값
 
-        return Math.max(xResult, yResult);
+        return maxCount;
     }
 
     private static Character[][] deepCopy(Character[][] original) {
