aa3be00ed3a377e9aa82cda2f4944a016e2e101b
yoon <cbj0010@naver.com>
2025-02-06 20:07:40 +0900

    10250 ACM호텔
    주석 추가

--- alth_Egger/src/Chapter01/P10250_ACM호텔/Main.java
+++ alth_Egger/src/Chapter01/P10250_ACM호텔/Main.java
@@ -8,12 +8,14 @@ public class Main {
         int allCount = scn.nextInt();
 
         while (allCount-- > 0) {
-            int y = scn.nextInt();
-            int x = scn.nextInt();
+            int y = scn.nextInt();  //층수
+            int x = scn.nextInt();  //호수
             int N = scn.nextInt();
 
             int floor = (N % y == 0) ? y : N % y;
+            // 층으로 나눈 값이 딱 떨어지면 젤 윗층인거 따라서 떨어지면 y아니면 나머지값
             int roomNumber = N % y == 0 ? N / y : N / y + 1;
+            // 층으로 나눈 값이 딱 떨어지면 방은 몫이랑 같음
             System.out.printf("%d%02d%n", floor, roomNumber);
 
         }
